b97bfb7262d45d8db83b7671c579c35c59a027fd
yassinabolnaga <158006282+yassinabolnaga@users.noreply.github.com>
2024-03-20 05:46:32 +0200

    01.php

---
+++ 13-19/01.php
@@ -0,0 +1,19 @@
+<?php $Elzero = "Elzero Courses" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content= <?="$Elzero";?>>
+    <title>Welcome To <?="$Elzero";?></title>
+  </head>
+  <body>
+    <h1><?="$Elzero";?></h1>
+    <p>Here In <?="$Elzero";?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?="$Elzero";?> Is The What You Need.</div>
+    <footer>All Right Reserved To <?="$Elzero";?></footer>
+  </body>
+</html>
