fde3fbc9115f371474abb2cef557c4ca7f4c3b7f
yassinabolnaga <158006282+yassinabolnaga@users.noreply.github.com>
2024-03-20 18:06:41 +0200

    Create 04.php

---
+++ 20-29/04.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 10;
+$b = 20;
+$c = 15;
+
+var_dump($a < $b); // True
+echo '<br>';
+var_dump($c >= $a); // True
+echo '<br>';
+var_dump($a and $b); // True
+echo '<br>';
+var_dump($a or $b); // True
+echo '<br>';
+var_dump($a <> $c); // True
+echo '<br>';
+var_dump($a != $c); // True
+echo '<br>';
+var_dump(gettype($a) == gettype($b)); // True
+echo '<br>';
+var_dump(gettype($a) === gettype($b)); // True
+echo '<br>';
+var_dump(gettype((float) $a) !== gettype($b)); // True
