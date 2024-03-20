61e745c162612f05dc72b1bc75158274ba39534d
yassinabolnaga <158006282+yassinabolnaga@users.noreply.github.com>
2024-03-20 18:21:06 +0200

    Update 08.php

--- 20-29/08.php
+++ 20-29/08.php
@@ -1,10 +1,10 @@
 <?php
 
 // Code 1
-$a = @$b;
+$a = @$b or die("Custom Error");
 
 // Code 2
-$f = @file("Not_A_File");
+$f = @file("Not_A_File") or die("Custom Error");
 
 // Code 3
-@include("Not_A_File");
+@include("Not_A_File") or die("Custom Error");
