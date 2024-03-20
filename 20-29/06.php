c7faf06a4ab156855c7bd939c7b857b76934fb8c
yassinabolnaga <158006282+yassinabolnaga@users.noreply.github.com>
2024-03-20 18:18:14 +0200

    Create 06.php

---
+++ 20-29/06.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = "Elzero";
+$b = "Web";
+$c = "School";
+
+// Method 1
+// $d = "$a $b $c";
+
+// Method 2
+// $d= "{$a} {$b} {$c}";
+
+// Method 3
+// $d = $a ." ". $b ." ". $c;
+
+// Method 4
+// $d ="{$a} {$b} " .$c;
+
+echo $d;
