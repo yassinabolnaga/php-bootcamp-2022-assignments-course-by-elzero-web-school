b461887827277ed4f1f688b0159eeaddfb973d4b
yassinabolnaga <158006282+yassinabolnaga@users.noreply.github.com>
2024-03-21 07:29:24 +0200

    Create 04.php

---
+++ 37-42/04.php
@@ -0,0 +1,16 @@
+<?php
+
+$start = 10;
+
+for ($start = 10; $start <=10; $start--){
+
+    if ($start < 10){
+        echo "0";
+    }
+    echo $start;
+    echo'<br>';
+
+    if ($start == 3){
+        break;
+    }
+}
