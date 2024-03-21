062690bc19a0c3cfa748d70189da3f9aaa0ef7d4
yassinabolnaga <158006282+yassinabolnaga@users.noreply.github.com>
2024-03-21 07:52:05 +0200

    Create 06.php

---
+++ 37-42/06.php
@@ -0,0 +1,9 @@
+<?php
+
+$money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
+
+// foreach ($array as $key => $value)  AKV
+foreach ($money as $name => $need){
+
+    echo "The Name Is $name And I Need $need Pound From Him <br>";
+}
