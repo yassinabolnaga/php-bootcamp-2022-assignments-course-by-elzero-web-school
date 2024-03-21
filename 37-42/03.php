5867dfcd5df9b12f31ba03131d4b8de22f598ba4
yassinabolnaga <158006282+yassinabolnaga@users.noreply.github.com>
2024-03-21 07:24:20 +0200

    Create 03.php

---
+++ 37-42/03.php
@@ -0,0 +1,15 @@
+<?php
+
+$num = 2;
+while ($num < 520) {
+    if ($num == 382) {
+        break;
+    }
+    if ($num == 2) {
+        $num--;
+        echo "1";
+    }
+    echo "<br>";
+    echo $num = (2 * $num) + 2;
+
+}
