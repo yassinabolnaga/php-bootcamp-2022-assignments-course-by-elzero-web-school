c141f6d1a232f26e7fc9187c474aed64d544553c
yassinabolnaga <158006282+yassinabolnaga@users.noreply.github.com>
2024-03-21 02:57:06 +0200

    Create 08.php

---
+++ 30-36/08.php
@@ -0,0 +1,36 @@
+<?php 
+
+$day = "";
+
+    if($day === "Sat")
+    {
+        echo "We Are Open All The Day";
+    }
+    elseif($day === "Sun")
+    {
+        echo "We Are Open All The Day";
+    }
+    elseif($day === "Mon")
+    {
+        echo "We Are Open All The Day";
+    }
+    elseif($day === "Tue")
+    {
+        echo "We Are Open From 08:12";
+    }
+    elseif($day === "Wed")
+    {
+        echo "We Are Open From 08:12";
+    }
+    elseif($day === "Thu")
+    {
+        echo "We Are Closed";
+    }
+    elseif($day === "Fri")
+    {
+        echo "We Are Closed";
+    }
+    else
+    {
+        echo "Unknown Day";
+    }
